b63af60d43c787a36e6aa9344b26203f5d9f01a6
Namjimkai52 <soranathkonde@gmail.com>
2024-12-20 23:25:55 +0700

    already and ta check

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using  namespace std ;
+
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    int i = 0;
+    while(i < 3 ){
+       cin.get();
+       i++;
+    }
+    srand(time(0));
+    int m = rand()%9;
+    if(m == 0)  cout << "You will get A in this 261102.";
+    else if (m == 1)  cout << "You will get B+ in this 261102.";
+    else if (m == 2) cout << "You will get B in this 261102.";
+    else if (m == 3)cout << "You will get C+ in this 261102.";
+    else if (m == 4) cout << "You will get C in this 261102.";
+    else if (m == 5) cout << "You will get D+ in this 261102.";
+    else if (m == 6)cout << "You will get D in this 261102.";
+    else if (m == 7)cout << "You will get F in this 261102.";
+    else cout << "You will get W in this 261102.";
+
+
+    return 0;
+}
